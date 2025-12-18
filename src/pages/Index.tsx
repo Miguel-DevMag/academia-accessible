9c3e32387839d646d6da289a80ef06c99aba802d
miguel_DevMag <mangellaura006@gmail.com>
2025-12-17 22:45:16 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Layout } from '@/components/Layout';
 import { HeroSection } from '@/components/HeroSection';
 import { FeaturesSection } from '@/components/FeaturesSection';
+import { FeaturedWorkout } from '@/components/FeaturedWorkout';
 import { WorkoutsPreview } from '@/components/WorkoutsPreview';
 import { TestimonialsSection } from '@/components/TestimonialsSection';
 import { CTASection } from '@/components/CTASection';
@@ -11,8 +12,7 @@ const Index = () => {
     <Layout>
       {/* SEO Meta tags são gerenciadas pelo index.html */}
       <HeroSection />
-      <FeaturesSection />
-      <WorkoutsPreview />
+      <FeaturesSection />      <FeaturedWorkout />      <WorkoutsPreview />
       <TestimonialsSection />
       <CTASection />
     </Layout>
